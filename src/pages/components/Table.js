32552fb3177b11298a7ace8b780f06e686e3207b
KevinAlejandro17 <89487705+KevinAlejandro17@users.noreply.github.com>
2023-03-29 21:06:37 -0500

    Create course table

---
+++ src/pages/components/Table.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+
+const Table = () => {
+  const [items, setItems] = useState([]);
+
+  const getInfoAsignatura = (stringx) => {
+    let coincidencia = stringx.match(/ASIGNATURA: (\w+) (\w+) Gr.: (\d+)/);
+    if (coincidencia) {
+      return (
+        coincidencia[1] + " " + coincidencia[2] + " Grupo: " + coincidencia[3]
+      );
+    } else {
+      return null;
+    }
+  };
+
+  const readExcel = async (file) => {
+    const fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+
+    const data = await new Promise((resolve, reject) => {
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+
+        const wsname = wb.SheetNames[0];
+
+        const ws = wb.Sheets[wsname];
+
+        const subject_data = XLSX.utils.sheet_to_json(ws);
+        const students_data = XLSX.utils.sheet_to_json(ws, { range: 3 });
+
+        const subject = subject_data[0];
+        const teacher = subject_data[1];
+
+        const asignatura = subject["SISTEMA DE REGISTRO ACADÉMICO Y ADMISIONES (SRA)"];
+        console.log(asignatura);
+        console.log(
+          teacher["SISTEMA DE REGISTRO ACADÉMICO Y ADMISIONES (SRA)"]
+        );
+        console.log("STUDENTS", students_data);
+        resolve(students_data);
+      };
+
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+
+    const mappedData = data.map((item) => ({
+      No: item["No."],
+      Código: item["Código"],
+      Nombre: item["Nombre Completo"],
+      Correo: item["Correo Institucional"],
+      Programa: item["Programa"],
+      T_Mat: item["T. Mat."],
+      Calificación: item["Calif."],
+    }));
+    setItems(mappedData);
+  };
+
+  const clearTable = () => {
+    setItems([]);
+    document.getElementById("file-upload").value = "";
+  };
+
+  return (
+    <div className="container">
+      <div className="upload-container">
+        <label htmlFor="file-upload" className="custom-file-upload">
+          Cargar datos asignatura
+        </label>
+        <input
+          id="file-upload"
+          type="file"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            readExcel(file);
+          }}
+        />
+        <button onClick={clearTable} className="custom-file-button">
+          Limpiar tabla
+        </button>
+      </div>
+
+      <div className="table-container">
+        <table className="styled-table">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Código</th>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Programa</th>
+              <th>T. Mat.</th>
+              <th>Calificación</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((d) => (
+              <tr key={d.No}>
+                <td>{d.No}</td>
+                <td>{d.Código}</td>
+                <td>{d.Nombre}</td>
+                <td>{d.Correo}</td>
+                <td>{d.Programa}</td>
+                <td>{d.T_Mat}</td>
+                <td>{d.Calificación}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
