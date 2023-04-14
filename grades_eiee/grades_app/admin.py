77127ce413db0d215d950d8277f6f7062f41e1ab
KevinAlejandro17 <89487705+KevinAlejandro17@users.noreply.github.com>
2023-04-14 05:10:33 -0500

    Frontend updated

--- grades_eiee/grades_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import SCC
-# Register your models here.
-
-admin.site.register(SCC)
--- grades_eiee/grades_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SCCView
-
-urlpatterns = [
-    path('scc/', SCCView.as_view(), name='scc'),
-    path('scc/<int:id>', SCCView.as_view(), name='scc_id'),
-]
